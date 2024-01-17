8d7207fcffa053600bf2a23e2f6a69172abe9db3
Tri Bach <tribachhumg@gmail.com>
2024-01-17 23:22:20 +0700

    Add Pet Project

--- user/src/main/java/petstore/user/controller/HealthCheck.java
+++ user/src/main/java/petstore/user/controller/HealthCheck.java
@@ -1,16 +1,13 @@
 package petstore.user.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/health")
 public class HealthCheck {
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
     public final String ok() {
         return "OK";
     }
