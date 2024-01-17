93d850aa4bfd63ba36f9e67b3f86b359115bddb4
Tri Bach <tribachhumg@gmail.com>
2024-01-17 23:22:20 +0700

    Add Pet Project

---
+++ pet/src/main/java/petstore/pet/HealthController.java
@@ -0,0 +1,17 @@
+package petstore.pet;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/health")
+@Log4j2
+@RequiredArgsConstructor
+public class HealthController {
+  @GetMapping
+  public ResponseEntity<Object> health(@RequestHeader String host) {
+    return ResponseEntity.ok().body("Hello Loan stupid! From " + host);
+  }
+}
