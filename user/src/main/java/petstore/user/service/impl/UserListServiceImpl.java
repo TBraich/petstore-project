d6d5869d082a6639cef2689a8241e1d1fd048a83
Tri Bach <tribachhumg@gmail.com>
2024-02-19 01:20:02 +0700

    User : Update User List service

--- user/src/main/java/petstore/user/service/impl/UserListServiceImpl.java
+++ user/src/main/java/petstore/user/service/impl/UserListServiceImpl.java
@@ -1,15 +1,14 @@
 package petstore.user.service.impl;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.List;
-import java.util.Properties;
+import static petstore.common.utils.KafkaUtils.USERS_TOPIC;
+
+import com.google.common.collect.Lists;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import petstore.user.dto.common.BasicResponse;
 import petstore.user.dto.user.request.CreateBatchUserRequest;
@@ -19,45 +18,42 @@ import petstore.user.service.UserListService;
 @RequiredArgsConstructor
 @Log4j2
 public class UserListServiceImpl implements UserListService {
-  private final ObjectMapper objectMapper;
   private final KafkaProducer<String, Object> kafkaProducer;
-
-  public static void main(String[] args) throws JsonProcessingException {
-    var properties = new Properties();
-
-    // Normal local kafka
-    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "0.0.0.0:9092");
-
-    // Producer config
-    properties.setProperty(
-        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-    properties.setProperty(
-        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
-    var kafkaProducer = new KafkaProducer<String, Object>(properties);
-
-    var userBatchRecords =
-        new ProducerRecord<String, Object>(
-            "users", new ObjectMapper().writeValueAsString(List.of("A", "B", "C")));
-
-    kafkaProducer.send(userBatchRecords);
-
-    kafkaProducer.flush();
-
-    kafkaProducer.close();
-  }
 
   @Override
   public BasicResponse create(String oneTimeId, CreateBatchUserRequest request) {
-    ProducerRecord<String, Object> userBatchRecords =
-        new ProducerRecord<>("users", request.getUsers());
+    // Separate to insert with 10 users per partition
+    var listBatch = Lists.partition(request.getUsers(), 10);
 
-    kafkaProducer.send(userBatchRecords);
+    // Prepare and sending records
+    for (int i = 0; i < listBatch.size(); i++) {
+      String key = "id-" + i;
+
+      ProducerRecord<String, Object> userBatchRecords =
+          new ProducerRecord<>(USERS_TOPIC, key, listBatch.get(i));
+
+      kafkaProducer.send(
+          userBatchRecords,
+          (recordMetadata, e) -> {
+            if (ObjectUtils.isNotEmpty(e)) {
+              log.error(
+                  "Failed sending records for {}, key: {} - Error: {}",
+                  recordMetadata.topic(),
+                  key,
+                  e.getMessage());
+            } else {
+              log.info(
+                  "Send records successfully! key: {}, partition: {}",
+                  key,
+                  recordMetadata.partition());
+            }
+          });
+    }
 
     kafkaProducer.flush();
 
     kafkaProducer.close();
 
-    return BasicResponse.builder().result("OK").build();
+    return BasicResponse.builder().result(HttpStatus.OK.name()).build();
   }
 }
