3ef82ba43834b983bb0902b3836bb87f89134890
Tri Bach <tribachhumg@gmail.com>
2024-02-19 01:18:37 +0700

    Common : Update Kafka config, utils

--- common/src/main/java/petstore/common/config/KafkaConfig.java
+++ common/src/main/java/petstore/common/config/KafkaConfig.java
@@ -1,27 +1,31 @@
 package petstore.common.config;
 
 import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
 public class KafkaConfig {
-  @Value("kafka.bootstrap-servers:0.0.0.0:9092")
+  @Value("${kafka.bootstrap-servers:0.0.0.0:9092}")
   private String bootstrapServers;
 
-  @Bean
-  public KafkaProducer<String, Object> newKafkaProducer() {
+  @Value("${kafka.group-id:user-application}")
+  private String groupId;
+
+  private Properties kafkaProperties() {
     var properties = new Properties();
 
     // Normal local kafka
     properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-
-    // Conduktor kafka
 
     // Producer config
     properties.setProperty(
@@ -29,6 +33,24 @@ public class KafkaConfig {
     properties.setProperty(
         ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
 
-    return new KafkaProducer<>(properties);
+    // Consumer config
+    properties.setProperty(
+        ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    properties.setProperty(
+        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class.getName());
+    properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+    properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+    return properties;
+  }
+
+  @Bean
+  public KafkaProducer<String, Object> newKafkaProducer() {
+    return new KafkaProducer<>(kafkaProperties());
+  }
+
+  @Bean
+  public KafkaConsumer<String, Object> newKafkaConsumer() {
+    return new KafkaConsumer<>(kafkaProperties());
   }
 }
---
+++ common/src/main/java/petstore/common/utils/KafkaUtils.java
@@ -0,0 +1,6 @@
+package petstore.common.utils;
+
+public class KafkaUtils {
+  public static final String USERS_TOPIC = "users";
+  public static final String USER_GROUP = "user-application";
+}
