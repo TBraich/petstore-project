a7ce9c34fae87eef2703ef6031cc5da7048225cf
Tri Bach <tribachhumg@gmail.com>
2024-03-03 01:28:06 +0700

    Common : Update AppUserDetailService, add save cache authorization info

--- common/src/main/java/petstore/common/config/detailservice/AppUserDetailService.java
+++ common/src/main/java/petstore/common/config/detailservice/AppUserDetailService.java
@@ -2,29 +2,40 @@ package petstore.common.config.detailservice;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import petstore.common.entity.AuthorizeInfo;
+import petstore.common.repository.CacheRepository;
 import petstore.common.service.AuthGateway;
 
 @Log4j2
 @Service
 @RequiredArgsConstructor
 public class AppUserDetailService implements UserDetailsService {
+  private static final String AUTH_ROLE = "ROLE";
   private final AuthGateway authGateway;
+  private final CacheRepository cacheRepository;
 
   @Override
   public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
-    // TODO: implement checking with cache before querying DB
+    var authData = cacheRepository.find(AUTH_ROLE, userId, AuthorizeInfo.class);
 
-    var user = authGateway.authorize(userId);
+    if (ObjectUtils.isEmpty(authData)) {
+      authData = authGateway.authorize(userId);
+
+      // TODO: implement check path APIs with Role value
+
+      cacheRepository.set(AUTH_ROLE, userId, authData);
+    }
 
     return new User(
-        user.getId(),
-        "default",
-        user.getRoles().stream().map(SimpleGrantedAuthority::new).toList());
+        authData.getId(),
+        "unused",
+        authData.getRoles().stream().map(SimpleGrantedAuthority::new).toList());
   }
 }
