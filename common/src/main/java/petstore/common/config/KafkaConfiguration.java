9d1d390ef7b8c8487a6f55504f26763d0400be3d
Tri Bach <tribachhumg@gmail.com>
2024-03-09 23:47:50 +0700

    Common : Remove Kafka topic config

--- common/src/main/java/petstore/common/config/KafkaConfiguration.java
+++ common/src/main/java/petstore/common/config/KafkaConfiguration.java
@@ -35,8 +35,6 @@ public class KafkaConfiguration {
     config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
 
     // Consumer config
-    // TODO: Implement handle groupId
-    config.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
     config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
     config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class.getName());
     config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
